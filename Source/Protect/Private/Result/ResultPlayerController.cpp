9568c2ba9bcacdec490db6d214d2afa9bb434b7e
Tongari-Boy <tongariboy@outlook.com>
2026-09-08 14:41:02 +0900

    画面遷移0リザルトレベルへ遷移

---
+++ Source/Protect/Private/Result/ResultPlayerController.cpp
@@ -0,0 +1,36 @@
+﻿#include "Result/ResultPlayerController.h"
+
+#include <EnhancedInputComponent.h>
+#include <EnhancedInputSubsystems.h>
+#include <Kismet\GameplayStatics.h>
+
+
+void AResultPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (ULocalPlayer* LP = GetLocalPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* SubSystem = 
+			LP -> GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			SubSystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+
+}
+
+void AResultPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		EIC->BindAction(StartAction, ETriggerEvent::Started, this, &AResultPlayerController::HandeReturn);
+	}
+}
+
+void AResultPlayerController::HandeReturn(const FInputActionValue& Value)
+{
+	UGameplayStatics::OpenLevel(this, TitleLevelName);
+}
---
+++ Source/Protect/Public/Result/ResultPlayerController.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "InputMappingContext.h"
+
+#include "ResultPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTECT_API AResultPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* StartAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameFlow")
+	FName TitleLevelName = "Lvl_Title";
+
+	/**
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<class UResultWidget> ResultWidgetClass;
+	*/
+
+	void HandeReturn(const FInputActionValue& Value);
+};
